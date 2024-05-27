a1088a9d14808256f0ad0d333145405a405ea4f0
Fabri Cost <you@example.com>
2024-05-27 11:00:34 -0300

    Se agrego manejo de errores

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ import os
 
 url = 'https://es.dragon-ball-official.com/'
 
-response = requests.get(url)
+try:
+    response = requests.get(url)
+    response.raise_for_status()  # Verificar si hay errores en la respuesta HTTP
+except requests.exceptions.RequestException as e:
+    print(f'Error de conexión: {e}')
+    exit(1)
+
 soup = BeautifulSoup(response.text, 'html.parser')
 results = soup.find_all("img")
 image_links = []
@@ -21,6 +27,17 @@ for i, img in enumerate(results):
     file_extension = link.split('.')[-1].split('?')[0]
     if file_extension not in ['jpg', 'png', 'webp']:
         continue
-    response = requests.get(link)
-    with open('imagenes/img{}.{}'.format(i, file_extension), 'wb') as file:
-        file.write(response.content)
+
+    try:
+        response = requests.get(link)
+        response.raise_for_status()  # Verificar si hay errores en la respuesta HTTP
+    except requests.exceptions.RequestException as e:
+        print(f'Error al descargar la imagen {link}: {e}')
+        continue
+
+    try:
+        with open(f'imagenes/img{i}.{file_extension}', 'wb') as file:
+            file.write(response.content)
+    except IOError as e:
+        print(f'Error al guardar la imagen {link}: {e}')
+        continue
